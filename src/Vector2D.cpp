97e7ec279eaf27d8c85263ef2ccc1ac46b929b86
Amar Saggu <amar.singh.saggu@gmail.com>
2015-06-11 23:02:07 +0100

    Added operations to the Vector2D class.
    Overloaded some more useful operators for 2D vector manipulation

--- src/Vector2D.cpp
+++ src/Vector2D.cpp
@@ -1,19 +1,29 @@
 #include "Vector2D.hpp"
 
+#include <cmath>
+
 Vector2D::Vector2D()
 : x(0), y(0)
 {}
 
-Vector2D::Vector2D(int x, int y)
+Vector2D::Vector2D(double x, double y)
 : x(x), y(y)
 {}
 
+double Vector2D::GetLength() const
+{
+	return std::sqrt(x*x + y*y);
+}
+
+double Vector2D::GetDistance(const Vector2D &vec) const
+{
+	return sqrt(pow(x-vec.x, 2.0) + pow(y-vec.y, 2.0));
+}
 
 bool Vector2D::operator==(const Vector2D &vec)
 {
 	return (x == vec.x && y == vec.y);
 }
-
 bool Vector2D::operator!=(const Vector2D &vec)
 {
 	return !operator==(vec);
@@ -24,9 +34,37 @@ void Vector2D::operator+=(const Vector2D &vec)
 	x += vec.x;
 	y += vec.y;
 }
-
 void Vector2D::operator-=(const Vector2D &vec)
 {
 	x -= vec.x;
 	y -= vec.y;
+}
+
+void Vector2D::operator*=(double f)
+{
+	x *= f;
+	y *= f;
+}
+void Vector2D::operator/=(double f)
+{
+	x /= f;
+	y /= f;
+}
+
+Vector2D Vector2D::operator+(const Vector2D &vec)
+{
+	return {x + vec.x, y + vec.y};
+}
+Vector2D Vector2D::operator-(const Vector2D &vec)
+{
+	return {x - vec.x, y - vec.y};
+}
+
+Vector2D Vector2D::operator*(double f)
+{
+	return {x * f, y * f};
+}
+Vector2D Vector2D::operator/(double f)
+{
+	return {x / f, y / f};
 }
--- src/Vector2D.hpp
+++ src/Vector2D.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
 struct Vector2D {
-	int x, y;
+	double x, y;
 	
 	Vector2D();
-	Vector2D(int x, int y);
+	Vector2D(double x, double y);
+
+	double GetLength() const;
+	
+	double GetDistance(const Vector2D &vec) const;
 
 	bool operator==(const Vector2D &vec);
 	bool operator!=(const Vector2D &vec);
 	void operator+=(const Vector2D &vec);
 	void operator-=(const Vector2D &vec);
+	
+	void operator*=(double f);
+	void operator/=(double f);
+	
+	Vector2D operator+(const Vector2D &vec);
+	Vector2D operator-(const Vector2D &vec);
+	
+	Vector2D operator*(double f);
+	Vector2D operator/(double f);
 };
